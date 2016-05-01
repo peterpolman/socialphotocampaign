bc83daafa61a574d1b6cafc5bbba635d070f0c28
Peter Polman <info@peterpolman.nl>
2016-05-01 11:03:40 +0200

    Finetuning

--- sites/all/themes/opknappertje/templates/node/node--contribution.tpl.php
+++ sites/all/themes/opknappertje/templates/node/node--contribution.tpl.php
@@ -103,12 +103,21 @@ if ($teaser) {
 <?php
 } else {
 ?>
+<?php
+sdpm($variables);
 
-<h3>Het verhaal achter de keuken van: <br><span>Peter Polman</span></h3>
+
+?>
+
+<h3>Het verhaal achter de keuken van: <br><span>
+  <?php print $variables['user_profile']['first_name']; ?>
+  <?php print $variables['user_profile']['last_name']; ?>
+  </span>
+ </h3>
 <p><i><?php print $variables['contribution']['body']; ?></i></p>
-<h3>Wil je Peter helpen met het verzamelen van stemmen?</h3>
-<p><a class="share btn" href="http://www.facebook.com/sharer.php?u=http://opknappertjenodig.nl/keukens/opknapper/407" target="_blank">Deel op Facebook!</a>
-</p><p>Gebruik de bovenstaande knop om de link van Peter op je Facebook tijdlijn te plaatsen. Zo roep je niet alleen de vrienden van Peter, maar ook jouw vrienden op om mee te helpen.</p>
+<h3>Wil je <?php print $variables['user_profile']['first_name']; ?> helpen met het verzamelen van stemmen?</h3>
+<p><a class="btn btn-facebook" href="http://www.facebook.com/sharer.php?u=http://opknappertjenodig.nl/keukens/opknapper/407" target="_blank">Deel op Facebook!</a>
+</p><p>Gebruik de bovenstaande knop om de link van <?php print $variables['user_profile']['first_name']; ?> op je Facebook tijdlijn te plaatsen. Zo roep je niet alleen de vrienden van Peter, maar ook jouw vrienden op om mee te helpen.</p>
 
 <?php
 }
